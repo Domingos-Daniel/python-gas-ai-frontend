6c0fedab99903da0c95b11bac4c4761feb9dac93
Domingos-Daniel <domingoscahandadaniel@gmail.com>
2025-07-20 16:37:04 +0100

    fix: remove trailing slash from API URL

--- src/components/ChatInterface.tsx
+++ src/components/ChatInterface.tsx
@@ -101,7 +101,7 @@ export default function ChatInterface() {
     setIsLoading(true);
 
     try {
-      const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+      const apiUrl = (process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000").replace(/\/$/, "");
       const response = await fetch(`${apiUrl}/chat`, {
         method: "POST",
         headers: {
